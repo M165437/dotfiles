180166ce7472cd165a92da0acd8aa3a59820e7fd
Michael Schmidt-Voigt <michael@schmidt-voigt.de>
2017-09-20 17:51:56 -0300

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -5,13 +5,14 @@ alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias lsize="ls -lS"
 alias weather="curl -4 http://wttr.in"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias myip='ipconfig getifaddr en1'
 
 # Directories
-alias dotfiles="cd $DOTFILES"
+alias dotfiles="cd $DOTFILES && stt ."
 
 # Laravel
 alias pa="php artisan"
